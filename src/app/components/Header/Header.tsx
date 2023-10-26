8a0db1ada12ee7103b1e7d4d55822b0a9d0a6999
Raushaniia <mullakhmetova18@gmail.com>
2023-10-26 18:13:35 +0400

    feat: add variables for styles and create a dictionary

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,11 +4,13 @@ import "./Header.scss";
 import {SiteName} from "./SiteName/SiteName";
 import {Menu} from "./Menu/Menu";
 import {CurrentDate} from "./CurrentDate/CurrentDate";
+import {Navigation} from "./Navigation/Navigation";
 
 export const Header: FC<IHeader> = ({name, title}) => {
     return <div className="Header">
         <SiteName name={name}/>
         <CurrentDate />
         <Menu title={title} />
+        <Navigation />
     </div>
 }
---
+++ src/app/components/Header/Navigation/Navigation.tsx
@@ -0,0 +1,15 @@
+import React, {FC} from "react";
+import {INavigation} from "./INavigation";
+import "./Navigation.scss";
+
+export const Navigation: FC<INavigation> = () => {
+    return <div className="Navigation">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <button></button>
+    </div>
+}
