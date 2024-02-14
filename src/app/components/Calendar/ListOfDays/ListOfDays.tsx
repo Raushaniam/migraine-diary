30961fadacac8b877cb76726e12f61dae0d70070
Raushaniia <mullakhmetova18@gmail.com>
2024-02-14 18:06:07 +0400

    fix: exclude duplicate keys for days in ListOfDays

--- src/app/components/Calendar/ListOfDays/ListOfDays.tsx
+++ src/app/components/Calendar/ListOfDays/ListOfDays.tsx
@@ -4,11 +4,13 @@ import './ListOfDays.scss'
 
 export const ListOfDays: FC<IListOfDays> = ({month, currentDay}) => {
     const listOfDays: ReactNode[] = month.map((item, index) => {
+        let i = 0;
         return item.map((day) => {
+            i++;
             if (day.date === 0) {
-                return <div key={index} className="Day Pointer"></div>;
+                return <div key={i} className="Day Pointer"></div>;
             }
-            return <div key={index} className={day.date === currentDay ? "Day Pointer CurrentDay" : "Day Pointer"}>
+            return <div key={i} className={day.date === currentDay ? "Day Pointer CurrentDay" : "Day Pointer"}>
                 {day.date}
             </div>;
         })
