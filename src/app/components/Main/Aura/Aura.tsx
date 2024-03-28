592072a33044cd1dcd3590ea00a795d0b5fd4c15
Raushaniia <mullakhmetova18@gmail.com>
2024-03-28 13:19:43 +0400

    feat: implement radio buttons for Aura

--- src/app/components/Main/Aura/Aura.tsx
+++ src/app/components/Main/Aura/Aura.tsx
@@ -1,17 +1,25 @@
-import React, {FC} from "react";
+import React, {FC, useState} from "react";
 import {IAura} from "./IAura";
 import "./Aura.scss";
 
 export const Aura: FC<IAura> = ({title, yes, no}) => {
+    const [activeButton, setActiveButton] = useState<boolean>(false);
+
+    const chooseButton = () => {
+        setActiveButton(!activeButton);
+    }
+
     return <div className="Aura">
         <div className="Title">{title}</div>
         <div className="AuraContainer">
-            <label className="Yes" htmlFor="inputYes">{yes}
-                <input className="Input" type="radio" id="inputYes"/>
-            </label>
-            <label className="No" htmlFor="inputNo">{no}
-                <input className="Input" type="radio" id="inputNo"/>
-            </label>
+            <div className={activeButton ? "Active Yes" : "Yes"}
+                 onClick={chooseButton}>
+                {yes}
+            </div>
+            <div className={!activeButton ? "Active No" : "No"}
+                 onClick={chooseButton}>
+                {no}
+            </div>
         </div>
     </div>
 }
